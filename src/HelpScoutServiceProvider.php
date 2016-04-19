ee5c4cf363dec52c91e39f39146287d071f101a6
Iain <iain@polevaultweb.com>
2016-04-19 13:56:56 +0100

    Add service provider

---
+++ src/HelpScoutServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Polevaultweb\LaravelSparkHelpScout;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelpScoutServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
